2abcd706e1fb81da6e77af06c9ba7c2f2cec3cee
Niccole Riera <nriera@utexas.edu>
2022-05-09 16:30:00 -0500

    corrected a typo

--- src/jobs.py
+++ src/jobs.py
@@ -22,7 +22,28 @@ def generate_jid():
 def generate_job_key(jid):
     """
     """
-    return 'job.{}'.format(jid)
+    return 'job.{  rray([rmag*np.cos(nu), rmag*np.sin(nu),0])
+    p=sma*(1-emag**2)
+    v= np.array([-np.sin(nu)*np.sqrt(mu/p), (emag +np.cos(nu))*np.sqrt(mu/p), 0])
+    r1 = np.array([[np.cos(bw), -np.sin(bw), 0],[np.sin(bw), np.cos(bw), 0],[0,0,1]])
+    r2 = np.array([[1,0,0],[0, np.cos(i), -np.sin(i)], [0, np.sin(i), np.cos(i)]])
+    r3 = np.array([[np.cos(sw), -np.sin(sw), 0],[np.sin(sw), np.cos(sw),0],[0,0,1]])
+    rotm = np.matmul(r1,r2)
+    rotm = np.matmul(rotm,r3)
+    rijk = rotm.dot(r)
+    vijk = rotm.dot(v)
+    E= np.arccos(abs(r[0]/sma))
+  #print(r[0]/sma)
+    M2 = E*-emag*np.sin(E)
+    M2=-M2
+    rcurr=np.zeros((3,1))
+    err=abs(M2-M)
+    rcurr[0]=rijk[0]
+    rcurr[1]=rijk[1]
+    rcurr[2]=rijk[2]
+
+
+    return [rijk[0],rijk[1],rijk[2],vijk[0],vijk[1],vijk[2],rcurr,err]}'.format(jid)
 
 def instantiate_job(jid, status, start, end):
     """
