531c5367ce851f4673ffc7ece6a6d5ccded8ffcf
Niccole Riera <nriera@utexas.edu>
2022-05-09 16:14:45 -0500

    added more jobs functions

--- src/jobs.py
+++ src/jobs.py
@@ -1,16 +1,17 @@
 import json
 import os
 import uuid
-from flask import Flask, request, send_file
 import redis
+import numpy as np
+import matplotlib.pyplot as plt 
 from hotqueue import HotQueue
 
-redis_ip = os.environ.get('REDIS_IP')
-if not redis_ip:
-    raise Exception()
-
-rd = redis.Redis(host=redis_ip, port=6379, db=0)
-q = HotQueue("queue"m host=redis_ip, port=6379, db=1)
+#redis_ip = os.environ.get('REDIS_IP')
+#if not redis_ip:
+#    raise Exception()
+redis_ip = '172.17.0.4'
+rd = redis.Redis(host=redis_ip, port='6379', db=0)
+q = HotQueue("queue", host=redis_ip, port='6379', db=1)
 
 def generate_jid():
     """
@@ -23,7 +24,7 @@ def generate_job_key(jid):
     """
     return 'job.{}'.format(jid)
 
-def instantiate_job(jid, start, end):
+def instantiate_job(jid, status, start, end):
     """
     This function creates a python dictionary to store the job id, status, and start and end parameters.
     """
@@ -32,21 +33,55 @@ def instantiate_job(jid, start, end):
     return {'id': jid.decode('utf-8'), 'status': status.decode('utf-8'), 'start': start.decode('utf-8'), 'end': end.decode('utf-8')}
 
 def save_job(job_key, job_dict):
-    rd.hset(job_key, mapping=job_dict)
+    rd.set(job_key, job_dict)
 
 def queue_job(jid):
     q.put(jid) 
 
-def add_job(start, end, status="submitted"):
+def add_job(start, end):
     jid = generate_jid()
-    job_dict = instantiate_job(jid, status, start, end)
-    save_job(generate_job_key(jid), job_dict) 
+    job_dict = instantiate_job(jid, "submitted", start, end)
+    save_job(generate_job_key(jid), json.dumps(job_dict)) 
     queue_job(jid)
     return job_dict
 
-def updata_job_statues(jid, status):
-    job = get_job_by_id(jid)
+def update_job_status(jid, status):
+    job = json.loads(rd.get(generate_job_key(jid))) 
+
     if job:
         job['status'] = status
-        save_job(generate_job_key(jid), job)
+        save_job(generate_job_key(jid), json.dumps(job)) 
+    else:
+        raise Exception()
 
+def oe2rv(oe,mu,M):
+    d2r=np.pi/180
+    sma=oe[0]
+    emag=oe[1]
+    i=oe[2]*d2r
+    sw=oe[3]*d2r
+    bw=oe[4]*d2r
+    nu= oe[5]*d2r
+    rmag = sma*(1-emag**2)/(1+emag*np.cos(nu))
+    r=np.array([rmag*np.cos(nu), rmag*np.sin(nu),0])
+    p=sma*(1-emag**2)
+    v= np.array([-np.sin(nu)*np.sqrt(mu/p), (emag +np.cos(nu))*np.sqrt(mu/p), 0])
+    r1 = np.array([[np.cos(bw), -np.sin(bw), 0],[np.sin(bw), np.cos(bw), 0],[0,0,1]])
+    r2 = np.array([[1,0,0],[0, np.cos(i), -np.sin(i)], [0, np.sin(i), np.cos(i)]])
+    r3 = np.array([[np.cos(sw), -np.sin(sw), 0],[np.sin(sw), np.cos(sw),0],[0,0,1]])
+    rotm = np.matmul(r1,r2)
+    rotm = np.matmul(rotm,r3)
+    rijk = rotm.dot(r)
+    vijk = rotm.dot(v)
+    E= np.arccos(abs(r[0]/sma))
+  #print(r[0]/sma)
+    M2 = E*-emag*np.sin(E)
+    M2=-M2
+    rcurr=np.zeros((3,1))
+    err=abs(M2-M)
+    rcurr[0]=rijk[0]
+    rcurr[1]=rijk[1]
+    rcurr[2]=rijk[2]
+  
+  
+    return [rijk[0],rijk[1],rijk[2],vijk[0],vijk[1],vijk[2],rcurr,err]
