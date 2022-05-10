c4c7c905eb1bfa5b95a8936e402d9dde35d4e462
Niccole Riera <nriera@utexas.edu>
2022-05-10 03:21:57 -0500

    everything works

--- src/jobs.py
+++ src/jobs.py
@@ -2,8 +2,6 @@ import json
 import os
 import uuid
 import redis
-import numpy as np
-import matplotlib.pyplot as plt 
 from hotqueue import HotQueue
 
 #redis_ip = os.environ.get('REDIS_IP')
@@ -22,36 +20,16 @@ def generate_jid():
 def generate_job_key(jid):
     """
     """
-    return 'job.{  rray([rmag*np.cos(nu), rmag*np.sin(nu),0])
-    p=sma*(1-emag**2)
-    v= np.array([-np.sin(nu)*np.sqrt(mu/p), (emag +np.cos(nu))*np.sqrt(mu/p), 0])
-    r1 = np.array([[np.cos(bw), -np.sin(bw), 0],[np.sin(bw), np.cos(bw), 0],[0,0,1]])
-    r2 = np.array([[1,0,0],[0, np.cos(i), -np.sin(i)], [0, np.sin(i), np.cos(i)]])
-    r3 = np.array([[np.cos(sw), -np.sin(sw), 0],[np.sin(sw), np.cos(sw),0],[0,0,1]])
-    rotm = np.matmul(r1,r2)
-    rotm = np.matmul(rotm,r3)
-    rijk = rotm.dot(r)
-    vijk = rotm.dot(v)
-    E= np.arccos(abs(r[0]/sma))
-  #print(r[0]/sma)
-    M2 = E*-emag*np.sin(E)
-    M2=-M2
-    rcurr=np.zeros((3,1))
-    err=abs(M2-M)
-    rcurr[0]=rijk[0]
-    rcurr[1]=rijk[1]
-    rcurr[2]=rijk[2]
+    return 'job.{}'.format(jid)
 
 
-    return [rijk[0],rijk[1],rijk[2],vijk[0],vijk[1],vijk[2],rcurr,err]}'.format(jid)
-
-def instantiate_job(jid, status, start, end):
+def instantiate_job(jid, status, comet):
     """
     This function creates a python dictionary to store the job id, status, and start and end parameters.
     """
     if type(jid) == str:
-        return {'id': jid, 'status': status, 'start': start, 'end': end}
-    return {'id': jid.decode('utf-8'), 'status': status.decode('utf-8'), 'start': start.decode('utf-8'), 'end': end.decode('utf-8')}
+        return {'id': jid, 'status': status, 'comet': comet}
+    return {'id': jid.decode('utf-8'), 'status': status.decode('utf-8'), 'comet': comet.decode('utf-8')}
 
 def save_job(job_key, job_dict):
     rd.set(job_key, job_dict)
@@ -59,50 +37,31 @@ def save_job(job_key, job_dict):
 def queue_job(jid):
     q.put(jid) 
 
-def add_job(start, end):
+def add_job(comet):
     jid = generate_jid()
-    job_dict = instantiate_job(jid, "submitted", start, end)
+    job_dict = instantiate_job(jid, "submitted", comet)
     save_job(generate_job_key(jid), json.dumps(job_dict)) 
     queue_job(jid)
     return job_dict
 
 def update_job_status(jid, status):
-    job = json.loads(rd.get(generate_job_key(jid))) 
-
+    job = json.loads(rd.get(generate_job_key(jid)))
+    
     if job:
         job['status'] = status
         save_job(generate_job_key(jid), json.dumps(job)) 
     else:
-        raise Exception()
+        raise Exception() 
 
-def oe2rv(oe,mu,M):
-    d2r=np.pi/180
-    sma=oe[0]
-    emag=oe[1]
-    i=oe[2]*d2r
-    sw=oe[3]*d2r
-    bw=oe[4]*d2r
-    nu= oe[5]*d2r
-    rmag = sma*(1-emag**2)/(1+emag*np.cos(nu))
-    r=np.array([rmag*np.cos(nu), rmag*np.sin(nu),0])
-    p=sma*(1-emag**2)
-    v= np.array([-np.sin(nu)*np.sqrt(mu/p), (emag +np.cos(nu))*np.sqrt(mu/p), 0])
-    r1 = np.array([[np.cos(bw), -np.sin(bw), 0],[np.sin(bw), np.cos(bw), 0],[0,0,1]])
-    r2 = np.array([[1,0,0],[0, np.cos(i), -np.sin(i)], [0, np.sin(i), np.cos(i)]])
-    r3 = np.array([[np.cos(sw), -np.sin(sw), 0],[np.sin(sw), np.cos(sw),0],[0,0,1]])
-    rotm = np.matmul(r1,r2)
-    rotm = np.matmul(rotm,r3)
-    rijk = rotm.dot(r)
-    vijk = rotm.dot(v)
-    E= np.arccos(abs(r[0]/sma))
-  #print(r[0]/sma)
-    M2 = E*-emag*np.sin(E)
-    M2=-M2
-    rcurr=np.zeros((3,1))
-    err=abs(M2-M)
-    rcurr[0]=rijk[0]
-    rcurr[1]=rijk[1]
-    rcurr[2]=rijk[2]
-  
-  
-    return [rijk[0],rijk[1],rijk[2],vijk[0],vijk[1],vijk[2],rcurr,err]
+def add_key(jid, key, value):
+    job_dict = json.loads(rd.get(generate_job_key(jid)))
+    job_dict[key] = value
+    save_job(generate_job_key(jid), json.dumps(job_dict)) 
+
+def save_job_image(jid, value):
+    rd.hset('image.{}'.format(jid), 'image', value) 
+
+def get_comet_dict(jid):
+    job_dict = json.loads(rd.get(generate_job_key(jid)))
+    comet = job_dict['comet'] 
+    return json.loads(rd.get(str(comet))) 
