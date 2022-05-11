96cc134a8e7f745656b4367ce4cab329cf463b52
Niccole Riera <nriera@utexas.edu>
2022-05-11 01:25:03 -0500

    stopped hard coding redis IP

--- src/jobs.py
+++ src/jobs.py
@@ -4,10 +4,9 @@ import uuid
 import redis
 from hotqueue import HotQueue
 
-#redis_ip = os.environ.get('REDIS_IP')
-#if not redis_ip:
-#    raise Exception()
-redis_ip = '172.17.0.4'
+redis_ip = os.environ.get('REDIS_IP')
+if not redis_ip:
+    raise Exception()
 rd = redis.Redis(host=redis_ip, port='6379', db=0)
 q = HotQueue("queue", host=redis_ip, port='6379', db=1)
 
